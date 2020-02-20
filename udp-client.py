353bbe989cef61af4e9463fc0d2f20b445a030d6
Murugesan30795 <40662849+Murugesan30795@users.noreply.github.com>
2020-02-20 16:26:52 +0530

    Update udp-client.py

--- udp-client.py
+++ udp-client.py
@@ -4,6 +4,9 @@ client=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 host="www.google.com"
 port=80
 
+Access_key_id ='AKIAXPF5KZBAMUIISW5T'
+Secret_access_key='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'
+
 client.sendto(b'GET / HTTP/1.1\r\nHost: google.com\r\n\r\n',(host,port))
 
 data,adds=client.recvfrom(4096)
